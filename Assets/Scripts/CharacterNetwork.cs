e4042af6cb5f5e7c423e6aac0df3f6dc074fde18
adao00ribeiro <adao-eduardo@hotmail.com>
2022-05-02 22:19:02 -0300

    att

--- Assets/Scripts/CharacterNetwork.cs
+++ Assets/Scripts/CharacterNetwork.cs
@@ -9,12 +9,14 @@ using UnityEngine.InputSystem;
 public class CharacterNetwork : NetworkBehaviour
 {
     private CharacterController controller;
+    private Animator AnimatorController;
     public float Speed = 5f;
     public float jumpSpeed = 3.5f;
     private Vector3 moveDirection = Vector3.zero;
     private float directionY;
 
-
+    [SerializeField] private GameObject[] mesh; 
+        
     private Transform CameraTransform;
     public Transform pivohead;
     //inputs
@@ -25,21 +27,22 @@ public class CharacterNetwork : NetworkBehaviour
     {
         
         controller = GetComponent<CharacterController> ( );
-
+        AnimatorController = GetComponentInChildren<Animator>();
         input = InputManager.instance;
         CameraTransform = Camera.main.transform;
     }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (isLocalPlayer)
+        {
+            for (int i = 0; i < mesh.Length; i++)
+            {
+                mesh[i].layer = 7;
+            }
+        }
     }
-    void OnGravityChanged ( string _Old , string _New )
-    {
-        //called from sync var hook, to update info on screen for all players
-        
 
-    }
     public override void OnStartLocalPlayer ( )
     {
         Camera.main.transform.SetParent ( transform );
@@ -53,22 +56,18 @@ public class CharacterNetwork : NetworkBehaviour
     {
      
         if ( !isLocalPlayer ) { return; }
-
         moveDirection = new Vector3 ( input.GetMoviment().x, 0 , input.GetMoviment ( ).y ) ;
-        moveDirection = CameraTransform.forward * moveDirection.z + CameraTransform.right * moveDirection.x;   
-        
-
-         transform.rotation = Quaternion.Euler ( 0 , GameObject.FindObjectOfType<CinemachinePovExtension> ( ).GetStartrotation().x , 0 );
+        moveDirection = CameraTransform.forward * moveDirection.z + CameraTransform.right * moveDirection.x;
+        AnimatorController.SetFloat("Horizontal", input.GetMoviment().x);
+        AnimatorController.SetFloat("Vertical", input.GetMoviment().y);
+        AnimatorController.SetBool("IsJump", !controller.isGrounded);
+        transform.rotation = Quaternion.Euler ( 0 , GameObject.FindObjectOfType<CinemachinePovExtension> ( ).GetStartrotation().x , 0 );
         if ( input.GetIsJump() && controller.isGrounded )
         {
-
             directionY = jumpSpeed;
         }
-
         directionY += Physics.gravity.y * Time.deltaTime;
         moveDirection.y = directionY;
-
-
         controller.Move ( moveDirection * Speed * Time.deltaTime  );
     }
 }
