c80ad5a8bae9b0b954cdf563f530f29b328e3332
adao00ribeiro <adao-eduardo@hotmail.com>
2022-06-05 23:41:29 -0300

    att

--- Assets/Scripts/RotationOjects.cs
+++ Assets/Scripts/RotationOjects.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class RotationOjects : MonoBehaviour
 {
+    public string nameObject;
+    public float speed;
+    public bool IsOpen;
+    [SerializeField]private Vector3 QuaternionAberto;
+    [SerializeField]private Vector3 QuaternionFechado;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class RotationOjects : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if ( IsOpen )
+        {
+            transform.localRotation = Quaternion.RotateTowards ( transform.localRotation ,  Quaternion.Euler( QuaternionAberto ) , speed * Time.deltaTime );
+        }
+        else
+        {
+            transform.localRotation = Quaternion.RotateTowards ( transform.localRotation , Quaternion.Euler ( QuaternionFechado ) , speed * Time.deltaTime );
+        }
     }
 }
